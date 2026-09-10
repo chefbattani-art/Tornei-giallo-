8379176ec1e2d2d10f63314cb5d24c80db52f81f
chefbattani-art <chefbattani@gmail.com>
2026-09-10 02:16:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -59,164 +59,86 @@ def genera_calendario_corretto(portieri, attaccanti, num_turni, num_tavoli):
   a_list = list(attaccanti)
 
   turni_partite = []
-  compagni_precedenti = set()
-  avversari_precedenti = set()
-  ruoli_riposo_per_turno = {}
+  ruoli_riposo_per_turno = []
 
   is_portieri_in_eccesso = len(p_list) > len(a_list)
 
-  def risolvi_turno(t):
-    if t > num_turni:
-      return True
-
+  # 1. Generazione sequenziale dei turni regolari (da 1 a num_turni)
+  for t in range(1, num_turni + 1):
     p_curr = list(p_list)
     a_curr = list(a_list)
 
+    # Gestione del giocatore che riposa a rotazione in questo turno
     if is_portieri_in_eccesso:
-      idx_riposo_p = (t - 1) % len(p_curr)
-      portiere_in_riposo = p_curr.pop(idx_riposo_p)
-      ruoli_riposo_per_turno[t] = ("portiere", portiere_in_riposo)
+      idx_rip = (t - 1) % len(p_curr)
+      portiere_rip = p_curr.pop(idx_rip)
+      ruoli_riposo_per_turno.append(("portiere", portiere_rip))
     else:
-      idx_riposo_a = (t - 1) % len(a_curr)
-      attaccante_in_riposo = a_curr.pop(idx_riposo_a)
-      ruoli_riposo_per_turno[t] = ("attaccante", attaccante_in_riposo)
+      idx_rip = (t - 1) % len(a_curr)
+      attaccante_rip = a_curr.pop(idx_rip)
+      ruoli_riposo_per_turno.append(("attaccante", attaccante_rip))
 
-    for _ in range(1500):
-      p_temp = list(p_curr)
-      a_temp = list(a_curr)
-      random.shuffle(p_temp)
-      random.shuffle(a_temp)
+    random.shuffle(p_curr)
+    random.shuffle(a_curr)
 
-      coppie_questo_turno = set()
-      scontri_questo_turno = set()
-      partite_tentative = []
-      turno_valido = True
+    partite_turno = []
+    match_idx = 0
+    i = 0
+    while i < len(p_curr) and i + 1 < len(p_curr):
+      p1, a1 = p_curr[i], a_curr[i]
+      p2, a2 = p_curr[i + 1], a_curr[i + 1]
 
-      i = 0
-      while i < len(p_temp) and i + 1 < len(p_temp):
-        p1, a1 = p_temp[i], a_temp[i]
-        p2, a2 = p_temp[i + 1], a_temp[i + 1]
+      match_id = f"t{t}_m{match_idx}"
+      partite_turno.append({
+          "id": match_id,
+          "p1": p1,
+          "a1": a1,
+          "p2": p2,
+          "a2": a2,
+          "giocata": False,
+          "in_corso": False,
+          "gol1": 0,
+          "gol2": 0,
+      })
+      match_idx += 1
+      i += 2
 
-        squadra_1 = tuple(sorted([p1, a1]))
-        squadra_2 = tuple(sorted([p2, a2]))
+    # Aggiunta riga informativa del riposo per il turno corrente
+    tipo_rip, nome_rip = ruoli_riposo_per_turno[-1]
+    if tipo_rip == "attaccante":
+      partite_turno.append({
+          "id": f"t{t}_riposo_a",
+          "p1": "",
+          "a1": nome_rip,
+          "p2": "",
+          "a2": "",
+          "giocata": True,
+          "in_corso": False,
+          "gol1": 0,
+          "gol2": 0,
+          "è_riposo_attaccante": True,
+      })
+    else:
+      partite_turno.append({
+          "id": f"t{t}_riposo_p",
+          "p1": nome_rip,
+          "a1": "",
+          "p2": "",
+          "a2": "",
+          "giocata": True,
+          "in_corso": False,
+          "gol1": 0,
+          "gol2": 0,
+          "è_riposo_portiere": True,
+      })
 
-        if (
-            squadra_1 in compagni_precedenti
-            or squadra_1 in coppie_questo_turno
-            or squadra_2 in compagni_precedenti
-            or squadra_2 in coppie_questo_turno
-        ):
-          turno_valido = False
-          break
+    turni_partite.append({"turno": t, "partite": partite_turno})
 
-        giocatori_s1 = [p1, a1]
-        giocatori_s2 = [p2, a2]
-        conflitto_avversari = False
-        scontri_singoli = []
-
-        for g_a in giocatori_s1:
-          for g_b in giocatori_s2:
-            coppia_avversaria = tuple(sorted([g_a, g_b]))
-            if (
-                coppia_avversaria in avversari_precedenti
-                or coppia_avversaria in scontri_questo_turno
-            ):
-              conflitto_avversari = True
-              break
-            scontri_singoli.append(coppia_avversaria)
-          if conflitto_avversari:
-            break
-
-        if conflitto_avversari:
-          turno_valido = False
-          break
-
-        coppie_questo_turno.add(squadra_1)
-        coppie_questo_turno.add(squadra_2)
-        for c_avv in scontri_singoli:
-          scontri_questo_turno.add(c_avv)
-
-        partite_tentative.append({
-            "p1": p1,
-            "a1": a1,
-            "p2": p2,
-            "a2": a2,
-            "compagni": [squadra_1, squadra_2],
-            "avversari": scontri_singoli,
-        })
-        i += 2
-
-      if turno_valido:
-        for m_data in partite_tentative:
-          for comp in m_data["compagni"]:
-            compagni_precedenti.add(comp)
-          for avv in m_data["avversari"]:
-            avversari_precedenti.add(avv)
-
-        partite_turno = []
-        for match_idx, m_data in enumerate(partite_tentative):
-          match_id = f"t{t}_m{match_idx}"
-          partite_turno.append({
-              "id": match_id,
-              "p1": m_data["p1"],
-              "a1": m_data["a1"],
-              "p2": m_data["p2"],
-              "a2": m_data["a2"],
-              "giocata": False,
-              "in_corso": False,
-              "gol1": 0,
-              "gol2": 0,
-          })
-
-        tipo_rip, nome_rip = ruoli_riposo_per_turno[t]
-        if tipo_rip == "attaccante":
-          partite_turno.append({
-              "id": f"t{t}_riposo_a",
-              "p1": "",
-              "a1": nome_rip,
-              "p2": "",
-              "a2": "",
-              "giocata": True,
-              "in_corso": False,
-              "gol1": 0,
-              "gol2": 0,
-              "è_riposo_attaccante": True,
-          })
-        else:
-          partite_turno.append({
-              "id": f"t{t}_riposo_p",
-              "p1": nome_rip,
-              "a1": "",
-              "p2": "",
-              "a2": "",
-              "giocata": True,
-              "in_corso": False,
-              "gol1": 0,
-              "gol2": 0,
-              "è_riposo_portiere": True,
-          })
-
-        turni_partite.append({"turno": t, "partite": partite_turno})
-
-        if risolvi_turno(t + 1):
-          return True
-
-        turni_partite.pop()
-        for m_data in partite_tentative:
-          for comp in m_data["compagni"]:
-            compagni_precedenti.remove(comp)
-          for avv in m_data["avversari"]:
-            avversari_precedenti.remove(avv)
-        break
-
-    return False
-
-  risolvi_turno(1)
-
-  elementi_da_recuperare = [val[1] for val in ruoli_riposo_per_turno.values()]
-  if len(elementi_da_recuperare) > 0:
-    random.shuffle(elementi_da_recuperare)
+  # 2. Turno extra di recupero con i Jolly posizionato tassativamente alla fine
+  elementi_da_recuperare = [val[1] for val in ruoli_riposo_per_turno]
+  if elementi_da_recuperare:
     turno_num = num_turni + 1
+    random.shuffle(elementi_da_recuperare)
     partite_turno_extra = []
     match_idx = 0
 
@@ -224,19 +146,16 @@ def genera_calendario_corretto(portieri, attaccanti, num_turni, num_tavoli):
       attaccanti_jolly = list(attaccanti)
       random.shuffle(attaccanti_jolly)
       a_index = 0
-
       for i in range(0, len(elementi_da_recuperare), 2):
         if i + 1 < len(elementi_da_recuperare):
           p1_r = elementi_da_recuperare[i]
           p2_r = elementi_da_recuperare[i + 1]
-
           aj1 = attaccanti_jolly[a_index % len(attaccanti_jolly)]
           aj2 = attaccanti_jolly[(a_index + 1) % len(attaccanti_jolly)]
           a_index += 2
 
-          match_id = f"t{turno_num}_m{match_idx}"
           partite_turno_extra.append({
-              "id": match_id,
+              "id": f"t{turno_num}_m{match_idx}",
               "p1": p1_r,
               "a1": f"{aj1} (Jolly)",
               "p2": p2_r,
@@ -253,19 +172,16 @@ def genera_calendario_corretto(portieri, attaccanti, num_turni, num_tavoli):
       portieri_jolly = list(portieri)
       random.shuffle(portieri_jolly)
       p_index = 0
-
       for i in range(0, len(elementi_da_recuperare), 2):
         if i + 1 < len(elementi_da_recuperare):
           a1 = elementi_da_recuperare[i]
           a2 = elementi_da_recuperare[i + 1]
-
           pj1 = portieri_jolly[p_index % len(portieri_jolly)]
           pj2 = portieri_jolly[(p_index + 1) % len(portieri_jolly)]
           p_index += 2
 
-          match_id = f"t{turno_num}_m{match_idx}"
           partite_turno_extra.append({
-              "id": match_id,
+              "id": f"t{turno_num}_m{match_idx}",
               "p1": f"{pj1} (Jolly)",
               "a1": a1,
               "p2": f"{pj2} (Jolly)",
